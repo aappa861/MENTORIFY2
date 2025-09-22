0306c6a9ea34d477ec5d944c448738089ebb155e
om kurlekar <omkurlekar8@gmail.com>
2025-09-22 18:24:21 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  console.log('Hello from React!')
+  return (
+    <div>
+      <h1 className="text-red-500 text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
